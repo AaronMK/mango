4ff147c557c8b0eb340310c832fa0f7d792e40cd
Aaron Karp <aaron.karp@domenix.com>
2020-04-27 15:28:48 -0400

    Ability to problematically set alignment of mango-align element.

--- mango/align.js
+++ mango/align.js
@@ -15,64 +15,115 @@ export class MangoAlign extends HTMLElement
 			for (let i = 0; i < this.childNodes.length; ++i)
 				Children.push(this.childNodes[i]);
 
-			let ContainerElement = document.createElement('div');
-			ContainerElement.style.height = "auto";
-			ContainerElement.style.minHeight = "min-content";
-			ContainerElement.style.width = "auto";
-			ContainerElement.style.minWidth = "min-content";
+			this._container = document.createElement('div');
+			this._container.style.height = "auto";
+			this._container.style.minHeight = "min-content";
+			this._container.style.width = "auto";
+			this._container.style.minWidth = "min-content";
 
-			ContainerElement.append(...Children);
+			this._container.append(...Children);
 
 			this.style.display = "inline-grid";
+			this.style.boxSizing = "border-box";
+			this.style.margin = "0";
+
 			this.innerHTML = "";
 
 			if (this.hasAttribute("top"))
 			{
-				this.style.gridTemplateRows = "auto 1fr";
-				ContainerElement.style.gridRow = "1";
+				this.Align = MangoAlign.Top;
 			}
 			else if (this.hasAttribute("middle"))
 			{
-				this.style.gridTemplateRows = "1fr auto 1fr";
-				ContainerElement.style.gridRow = "2";
+				this.Align = MangoAlign.Middle;
 			}
 			else if (this.hasAttribute("bottom"))
 			{
-				this.style.gridTemplateRows = "1fr auto";
-				ContainerElement.style.gridRow = "2";
+				this.Align = MangoAlign.Bottom;
 			}
 			else
 			{
 				this.style.gridTemplateRows = "auto";
-				ContainerElement.style.gridRow = "1";
+				this._container.style.gridRow = "1";
 			}
+
+			/////////////////////////////////
 
 			if (this.hasAttribute("left"))
 			{
-				this.style.gridTemplateColumns = "auto 1fr";
-				ContainerElement.style.gridColumn = "1";
+				this.Align = MangoAlign.Left;
 			}
 			else if (this.hasAttribute("center"))
 			{
-				this.style.gridTemplateColumns = "1fr auto 1fr";
-				ContainerElement.style.gridColumn = "2";
+				this.Align = MangoAlign.Center;
 			}
 			else if (this.hasAttribute("right"))
 			{
-				this.style.gridTemplateColumns = "1fr auto";
-				ContainerElement.style.gridColumn = "2";
+				this.Align = MangoAlign.Right;
 			}
 			else
 			{
 				this.style.gridTemplateColumns = "auto";
-				ContainerElement.style.gridColumn = "1";
+				this._container.style.gridColumn = "1";
 			}
 
-			this.append(ContainerElement);
+			this.append(this._container);
 
 			this._built = true;
 		}
 	}
+
+	set Alignment(val)
+	{
+		if (val == MangoAlign.Left)
+		{
+			this._container.style.gridColumn = "1";
+			this.style.gridTemplateColumns = "auto 1fr";
+		}
+		else if (val == MangoAlign.Center)
+		{
+			this._container.style.gridColumn = "2";
+			this.style.gridTemplateColumns = "1fr auto 1fr";
+		}
+		else if (val == MangoAlign.Right)
+		{
+			this._container.style.gridColumn = "2";
+			this.style.gridTemplateColumns = "1fr auto";
+		}
+		else if (val == MangoAlign.Top)
+		{
+			this._container.style.gridRow = "1";
+			this.style.gridTemplateRows = "auto 1fr";
+		}
+		else if (val == MangoAlign.Middle)
+		{
+			this._container.style.gridRow = "2";
+			this.style.gridTemplateRows = "1fr auto 1fr";
+		}
+		else if (val == MangoAlign.Bottom)
+		{
+			this._container.style.gridRow = "2";
+			this.style.gridTemplateRows = "1fr auto";
+		}
+		else if (val == MangoAlign.None)
+		{
+			this.style.gridTemplateColumns = "auto";
+			this.style.gridTemplateRows = "auto";
+
+			this._container.style.gridColumn = "1";
+			this._container.style.gridRow = "1";
+		}
+	}
 }
+
+MangoAlign.None = 0;
+
+MangoAlign.Top = 1;
+MangoAlign.Middle = 2;
+MangoAlign.Bottom = 3;
+
+MangoAlign.Left = 4;
+MangoAlign.Center = 5;
+MangoAlign.Right = 6;
 
 customElements.define('mango-align', MangoAlign);
